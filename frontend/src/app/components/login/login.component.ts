6e4d44a4fe0ee32dfcebeb06bb1a70e5142d6238
marioolopez <150073715+marioolopez@users.noreply.github.com>
2025-08-12 18:08:23 +0200

    c

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { UsuarioService } from '../../services/usuario.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule, NgForm } from '@angular/forms'; // <-- IMPORTANTE
+import { empty } from 'rxjs';
 @Component({
   selector: 'app-login',
   imports: [CommonModule, FormsModule],
@@ -10,29 +11,46 @@ import { FormsModule, NgForm } from '@angular/forms'; // <-- IMPORTANTE
 })
 export class LoginComponent {
 
+  //variables para el registro
   public encR : boolean = false;
   public muestraEmlpe: boolean = false;
   public nomPersona: boolean = false;
 
+  //variables para el inicio de sesion
+  public encI: boolean = false;
+
   constructor(public usuario: UsuarioService){} //importar el servicio
 
   //solo del registro
-
   ocultarRegistro(){ //una vez iniciemos sesion
-     this.encR = false;
+     this.encR = !this.encR;
   }
-  registrarUsuario(recogerForm: NgForm): void {
-    if(!recogerForm.valid){
-      alert("Faltan datos por rellenar!");
+  registrarUsuario(recogerDatosRegistro: NgForm): void {
+    this.encI = false; //si le damos a registro y el inicio esta desplegado, lo cerramos.
+    if(!recogerDatosRegistro.valid){
+        alert("Faltan datos por rellenar!");
+        return
     }
     else{ this.usuario.crearUsuario(this.usuario.Usu).subscribe((usu) =>{
         alert("usuario creado");
-    });}
+      });
+    }
   }
 
 
   //inicio de sesion
-  inicioSesion(){
+  ocultarInicioSesion(){
+    this.encI = !this.encI;
+  }
+  inicioSesionUsu(recogerDatosInicio:NgForm){
+    this.encR = false;
+    if(!recogerDatosInicio.valid) {
+      alert("Faltan datos por rellenar!");
+      return;
+    }
+    else{
+      alert("Los campos estan llenos!");
+    }
 
   }
 }
